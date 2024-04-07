10dcf899aad81640bcdb9fbc19507daa9db4d48e
tsubasamanda <hamockingjay@gmail.com>
2024-04-06 22:25:44 -0400

    First commit w/ skeleton layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button } from "react-bootstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<p>
+				FFLogs link: <input></input>
+				<br />
+				<Button>Load</Button>
+			</p>
+			<table>
+				<tr>
+					<div id="line-1">
+						<p>
+							<td>
+								<Button>+</Button>
+							</td>
+							<td>100%</td>
+							<td>N0</td>
+							<td>B3 B4 PD F3 F4 F4 F4 PD F4 F4 F4 DSP</td>
+						</p>
+					</div>
+				</tr>
+			</table>
+		</div>
+	);
 }
 
 export default App;
